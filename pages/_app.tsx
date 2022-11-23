f224c4c1a5092651b0bb783169bd1866553493b5
Wendell Encorporado <brodwen83@gmail.com>
2022-11-23 14:14:47 +0800

    chore: use absolute imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
+import 'styles/globals.css';
 
-import { theme } from '../theme';
+import { theme } from 'theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
