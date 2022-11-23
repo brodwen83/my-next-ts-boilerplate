c7056826ddb7d5446079a710108b04a35d5f9b24
Wendell Encorporado <brodwen83@gmail.com>
2022-11-23 11:05:19 +0800

    build: add storybook and chakra-ui with storybook addon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "../theme";
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+
+import '../styles/globals.css';
+
+import { theme } from '../theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
